592eed5286d406325dab7ffe2e53e670b39b9d7f
UrbanRude <urban.rude@gmail.com>
2018-04-18 11:08:21 -0500

    Observable Avanzado

--- main.ts
+++ main.ts
@@ -6,24 +6,41 @@ let button = document.getElementById('button');
 let click = Observable.fromEvent(button,'click');
 
 function load(url:string){
-    let xhr = new XMLHttpRequest();
+    return Observable.create((observer) => {
+        let xhr = new XMLHttpRequest();
+        xhr.addEventListener('load',() =>{
+            if(xhr.status === 200){
+                let jsonStarwars = JSON.parse(xhr.responseText);
+                observer.next(jsonStarwars);
+                observer.complete();
+            }else{
+                observer.error(xhr.statusText);
+            }
+        });
+        xhr.open('GET',url);
+        xhr.send();
+    });
+    
+}
 
-    xhr.addEventListener('load',() =>{
-        let jsonStarwars = JSON.parse(xhr.responseText);
+function renderStarWars(jsonStarwars){
         jsonStarwars.forEach(element => {
             let div = document.createElement('div');
             div.innerText = element.name;
             output.appendChild(div);
         });
-    });
-    xhr.open('GET',url);
-    xhr.send();
-
 }
 
 click.subscribe(
     value => {
-        load('starwars.json');
+        load('starwars.json')
+            .subscribe(value=>{
+                        console.log(value);
+                        renderStarWars(value);
+                    },
+                        error=>{
+                            console.log(`Error >> ${error}`);
+                        });
     },
     error => {
         console.log(`Error`);
